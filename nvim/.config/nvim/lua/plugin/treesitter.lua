c7b3440948071553bcf857d692e52637e5444d69
Matt Bedell <matt@bedell.co>
2020-09-27 14:01:16 -0400

    add incremental selection default module values

--- nvim/.config/nvim/lua/plugin/treesitter.lua
+++ nvim/.config/nvim/lua/plugin/treesitter.lua
@@ -32,6 +32,15 @@ require'nvim-treesitter.configs'.setup{
       },
     },
   },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "gnn",
+      node_incremental = "grn",
+      scope_incremental = "grc",
+      node_decremental = "grm",
+    },
+  },
   textobjects = {
     select = {
       enable = true,
