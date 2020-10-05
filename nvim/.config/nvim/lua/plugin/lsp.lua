d08e86f59480dde842940882d4ce1956c90152fe
Matt Bedell <matt_bedell@condenast.com>
2020-10-05 12:54:55 -0400

    add diagnosticls language server for eslint, once multiple lsp diagnostics are working in nvim, replaces ALE

--- nvim/.config/nvim/lua/plugin/lsp.lua
+++ nvim/.config/nvim/lua/plugin/lsp.lua
@@ -6,6 +6,7 @@ on_attach_lsp = function(client)
   vim.fn.nvim_buf_set_keymap(0, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', {noremap = true, silent = true})
   vim.fn.nvim_buf_set_keymap(0, 'n', '<leader>lr', '<cmd>lua vim.lsp.buf.references()<CR>', {noremap = true, silent = true})
   vim.fn.nvim_buf_set_keymap(0, 'n', '<leader>lw', '<cmd>lua vim.lsp.buf.workspace_symbol()<CR>', {noremap = true, silent = true})
+  vim.fn.nvim_buf_set_keymap(0, 'n', '<leader>ls', '<cmd>lua vim.lsp.buf.document_symbol()<CR>', {noremap = true, silent = true})
   vim.fn.nvim_buf_set_keymap(0, 'n', ']e', '<cmd>NextDiagnosticCycle<CR>', {noremap = true, silent = true})
   vim.fn.nvim_buf_set_keymap(0, 'n', '[e', '<cmd>PrevDiagnosticCycle<CR>', {noremap = true, silent = true})
   vim.fn.nvim_buf_set_keymap(0, 'n', '<leader>le', '<cmd>lua vim.lsp.util.show_line_diagnostics()<CR>', {noremap = true, silent = true})
@@ -17,15 +18,62 @@ end
 
 nvim_lsp.tsserver.setup{
   root_dir = nvim_lsp.util.root_pattern(".git", "package.json"),
-  on_attach = on_attach_lsp
+  on_attach = on_attach_lsp,
 }
 
 nvim_lsp.pyls_ms.setup{
-  on_attach = on_attach_lsp
+  on_attach = on_attach_lsp,
 }
 nvim_lsp.rust_analyzer.setup{
-  on_attach = on_attach_lsp
+  on_attach = on_attach_lsp,
 }
+
+-- nvim_lsp.diagnosticls.setup{
+--   on_attach = on_attach_lsp,
+--   filetypes={'javascript'},
+--   init_options = {
+--     linters = {
+--       eslint = {
+--         command = './node_modules/.bin/eslint',
+--         rootPatterns = {'.git'},
+--         debounce = 100,
+--         args = {
+--           '--stdin',
+--           '--stdin-filename',
+--           '%filepath',
+--           '--format',
+--           'json',
+--         },
+--         sourceName = 'eslint',
+--         parseJson = {
+--           errorsRoot = '[0].messages',
+--           line = 'line',
+--           column = 'column',
+--           endLine = 'endLine',
+--           endColumn = 'endColumn',
+--           message = 'eslint: ${message} [${ruleId}]',
+--           security = 'severity',
+--         },
+--         securities = {
+--           [2] = 'error',
+--           [1] = 'warning',
+--         },
+--       },
+--     },
+--     filetypes = {
+--       javascript = 'eslint'
+--     },
+--     formatters = {
+--       prettier = {
+--         command = "./node_modules/.bin/prettier",
+--         args = {"--stdin-filepath" ,"%filepath", '--single-quote', '--print-width 120'},
+--       },
+--     },
+--     formatFiletypes = {
+--       javascript = "prettier",
+--     },
+--   },
+-- }
 
 vim.fn.sign_define("LspDiagnosticsErrorSign", {
   text = ">>",
