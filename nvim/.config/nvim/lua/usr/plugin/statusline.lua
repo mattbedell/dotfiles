889af6fe3b775b16d64257370bd18646e5f541c1
Matt Bedell <matt@bedell.co>
2020-11-07 23:18:57 -0500

    add diagnostic display to statusline

--- nvim/.config/nvim/lua/usr/plugin/statusline.lua
+++ nvim/.config/nvim/lua/usr/plugin/statusline.lua
@@ -1,10 +1,55 @@
 local vim = vim
+
+local lsp_diagnostics = require'lsp-status.diagnostics'
+local lsp_messages = require'lsp-status.messaging'.messages
+
 local usr_util = require'usr.util'
+local diagnostic_sign = require'usr.lsp'.diagnostic_sign
 
 local M = {}
 
+local function diagnostics()
+  local all_diagnostics = {}
+
+  if #vim.lsp.buf_get_clients() == 0 then
+    return ''
+  end
 
 
+  local buf_diagnostics = lsp_diagnostics()
+
+  if buf_diagnostics.errors and buf_diagnostics.errors > 0 then
+    all_diagnostics.error = buf_diagnostics.errors
+  end
+
+  if buf_diagnostics.warnings and buf_diagnostics.warnings > 0 then
+    all_diagnostics.warning = buf_diagnostics.warnings
+  end
+
+  if buf_diagnostics.info and buf_diagnostics.info > 0 then
+    all_diagnostics.info = buf_diagnostics.info
+  end
+
+  if buf_diagnostics.hints and buf_diagnostics.hints > 0 then
+    all_diagnostics.hint = buf_diagnostics.hint
+  end
+
+  local diagnostic_display = {}
+
+  local hiGroup = '%#Pmenu#'
+  for level, value in pairs(all_diagnostics) do
+    local hiLevel = '%#stlLsp' .. (level:gsub("^%l", string.upper)) .. '#'
+    -- diagnostic_display = diagnostic_display .. hiLevel .. ' ' .. value
+    table.insert(diagnostic_display, hiLevel ..  value )
+  end
+
+  if #diagnostic_display then
+    local spacer = hiGroup .. ' %*'
+    return '%4.10(' .. spacer .. usr_util.join(diagnostic_display, hiGroup .. ' %*') .. spacer .. '%)'
+  end
+
+  return ''
+end
 
 local function git()
   local stl_text = ''
@@ -29,21 +74,26 @@ local function status_inactive()
   ..' ' .. '%h%w%q'
   ..'%m'
   ..'%='
-  ..'%-10.(%l:%c%)'
-  ..'%-4.(%P%)'
+  ..'%-6.(%l:%c%)'
+  ..' %-4.(%P%)'
 
   vim.wo.statusline = stl_text
 end
 
 local function status_active()
+
+  -- @TODO: this is bad, refactor
+  local diagnostic_display = diagnostics()
+
   local stl_text = ' '
   ..buf_path(true)
   ..' ' .. '%h%w%q'
   .. git()
   ..'%#stlWarn#%m%*%r'
   ..'%='
-  ..'%-10.(%l:%c%)'
-  ..'%-4.(%P%)'
+  ..diagnostic_display
+  ..'%6.(%l:%c%)'
+  ..' %-4.(%P%)'
 
   vim.wo.statusline = stl_text
 end
@@ -55,6 +105,7 @@ usr_util.create_augroups({
   UsrStatusLine = {
     {'WinLeave', '*', [[lua require'usr.plugin.statusline'.status_inactive()]]},
     {'WinEnter,BufEnter', '*', [[lua require'usr.plugin.statusline'.status_active()]]},
+    {'User', 'LspDiagnosticsChanged', [[lua require'usr.plugin.statusline'.status_active()]]}
   }
 })
 
