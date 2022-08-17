ec17ffc661eed1f3ba14e6687e6b8c3a081c20b8
Matt Bedell <matt@bedell.co>
2022-08-17 11:18:50 -0400

    Disable treesitter indent for yaml files

--- nvim/.config/nvim/lua/usr/plugin/treesitter.lua
+++ nvim/.config/nvim/lua/usr/plugin/treesitter.lua
@@ -28,7 +28,7 @@ require'nvim-treesitter.configs'.setup{
   },
   indent = {
     enable = true,
-    disable = { "python", "rust" },
+    disable = { "python", "rust", "yaml" },
   },
   refactor = {
     highlight_definitions = { enable = true },
