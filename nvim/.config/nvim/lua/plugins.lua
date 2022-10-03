3d81f659c858fde75d1687d86303fa2f9a2afabb
Matt Bedell <matt@bedell.co>
2022-10-03 11:20:01 -0400

    Rm Plug plugins

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,55 +1,110 @@
+local packer_augroup = vim.api.nvim_create_augroup('packer_user_config', { clear = true })
+vim.api.nvim_create_autocmd('BufWritePost', {
+  group = packer_augroup,
+  pattern = 'plugins.lua',
+  callback = function(args)
+    vim.cmd('source ' .. args.file)
+    vim.cmd('PackerCompile')
+  end
+})
+
 vim.cmd [[packadd packer.nvim]]
 
-return require('packer').startup(function(use)
-  use {                                         -- fzf fuzzy finder wrapper
-    'junegunn/fzf',
-    requires = 'junegunn/fzf.vim',
-    ensure_dependencies = true,
-  }
-  use 'rhysd/git-messenger.vim'                               -- fugitive Blame is slow, this is faster
-  use 'iautom8things/gitlink-vim'                             -- generate a github link to current line
-  use 'gruvbox-community/gruvbox'                             -- theme
-  use 'lukas-reineke/indent-blankline.nvim'                   -- indent lines w/ treesitter context awareness
-  use 'windwp/nvim-autopairs'                                 -- autopairs
-  use {
-    'hrsh7th/nvim-cmp',                                       -- completion + completion sources
-    requires = {
-      'hrsh7th/cmp-buffer',
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-nvim-lsp-signature-help',
-      'hrsh7th/cmp-path',
-      'hrsh7th/cmp-vsnip',
-    },
-    ensure_dependencies = true,
-  }
-  use 'mfussenegger/nvim-dap'                                 -- debugger
-  use 'neovim/nvim-lspconfig'                                 -- convenient configs for language servers
-  use {                                                       -- abstraction layer for neovim's treesitter integration
-    'nvim-treesitter/nvim-treesitter',
-    requires = {
-      'nvim-treesitter/nvim-treesitter-refactor',
-      'nvim-treesitter/nvim-treesitter-textobjects',
-      'RRethy/nvim-treesitter-textsubjects',
-    },
-    ensure_dependencies = true,
-  }
-  use 'windwp/nvim-ts-autotag'                                -- treesitter aware auto-close html tags
-  use 'unblevable/quick-scope'                                -- highlight unique chars for 'f' and 't' motions
-  use 'wellle/targets.vim'                                    -- enhanced text objects
-  use 'vifm/vifm.vim'                                         -- vifm file manager
-  use {                                                       -- comment code
-    'tpope/vim-commentary',
-    requires = 'JoosepAlviste/nvim-ts-context-commentstring', -- update commentstring using treesitter for injected languages
-    ensure_dependencies = true,
-  }
-  use 'romainl/vim-cool'                                      -- auto highlight search, add search match count
-  use 'tpope/vim-dispatch'                                    -- async make
-  use 'tpope/vim-fugitive'                                    -- git integration
-  use 'yassinebridi/vim-purpura'                              -- theme, all purple because its fun
-  use 'vimjas/vim-python-pep8-indent'                         -- python treesitter indent is a WIP, use this until it's ready
-  use 'tpope/vim-repeat'                                      -- make mappings repeatable
-  use 'kshenoy/vim-signature'                                 -- visual marks in gutter TODO: replace this
-  use 'tpope/vim-surround'                                    -- mappings for surround characters
-  use 'fgsch/vim-varnish'                                     -- VCL syntax highlighting
-  use 'hrsh7th/vim-vsnip'                                     -- lsp snippet support
-end)
+local packer = require('packer')
+packer.init {
+  plugin_package = 'packer-plugins',
+}
+
+local use = packer.use
+packer.reset()
+
+use {                                                       -- fzf fuzzy finder wrapper
+  'junegunn/fzf.vim',
+  requires = 'junegunn/fzf',
+}
+use 'rhysd/git-messenger.vim'                               -- fugitive Blame is slow, this is faster
+use 'iautom8things/gitlink-vim'                             -- generate a github link to current line
+use 'gruvbox-community/gruvbox'                             -- theme
+use {                                                       -- indent lines w/ treesitter context awareness
+
+  'lukas-reineke/indent-blankline.nvim',
+  config = function()
+    require('vendor.plugin.nvim-autopairs')
+  end
+}
+use 'windwp/nvim-autopairs'                                 -- autopairs
+use {
+  'hrsh7th/nvim-cmp',
+  requires = {
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-nvim-lsp-signature-help',
+    'hrsh7th/cmp-path',
+    'hrsh7th/vim-vsnip',
+  },
+  config = function()
+    require('vendor.plugin.nvim-cmp')
+  end
+}
+use {
+  'nvim-treesitter/nvim-treesitter',
+  config = function()
+    require('usr.plugin.treesitter')
+  end
+}
+use {
+  'hrsh7th/cmp-vsnip',                                      -- nvim-cmp completion source
+  requires = {
+    'hrsh7th/vim-vsnip',                                    -- lsp snippet support
+  },
+  after = { 'nvim-cmp', 'vim-vsnip' },
+}
+use {                                                       -- debugger
+ 'mfussenegger/nvim-dap',
+  config = function()
+    require('vendor.plugin.nvim-dap')
+  end
+}
+use {                                 -- convenient configs for language servers
+  'neovim/nvim-lspconfig',
+  after = { 'cmp-nvim-lsp'},
+  config =function()
+    require('usr.plugin.lsp')
+  end
+}
+use {                                                       -- treesitter aware auto-close html tags
+  'windwp/nvim-ts-autotag',
+  after = 'nvim-treesitter',
+  requires = 'nvim-treesitter/nvim-treesitter',
+}
+use {
+  'nvim-treesitter/nvim-treesitter-refactor',
+  after = 'nvim-treesitter',
+  requires = 'nvim-treesitter/nvim-treesitter',
+}
+use {
+  'nvim-treesitter/nvim-treesitter-textobjects',
+  after = 'nvim-treesitter',
+  requires = 'nvim-treesitter/nvim-treesitter',
+}
+use {
+  'RRethy/nvim-treesitter-textsubjects',
+  after = 'nvim-treesitter',
+  requires = 'nvim-treesitter/nvim-treesitter',
+}
+use 'unblevable/quick-scope'                                -- highlight unique chars for 'f' and 't' motions
+use 'wellle/targets.vim'                                    -- enhanced text objects
+use 'vifm/vifm.vim'                                         -- vifm file manager
+use {                                                       -- comment code
+  'tpope/vim-commentary',
+}
+use 'JoosepAlviste/nvim-ts-context-commentstring'           -- update commentstring using treesitter for injected languages
+use 'romainl/vim-cool'                                      -- auto highlight search, add search match count
+use 'tpope/vim-dispatch'                                    -- async make
+use 'tpope/vim-fugitive'                                    -- git integration
+use 'yassinebridi/vim-purpura'                              -- theme, all purple because its fun
+use 'vimjas/vim-python-pep8-indent'                         -- python treesitter indent is a WIP, use this until it's ready
+use 'tpope/vim-repeat'                                      -- make mappings repeatable
+use 'kshenoy/vim-signature'                                 -- visual marks in gutter TODO: replace this
+use 'tpope/vim-surround'                                    -- mappings for surround characters
+use 'fgsch/vim-varnish'                                     -- VCL syntax highlighting
