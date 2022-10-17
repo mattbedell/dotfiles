c8dfa66d1f333501d68ffcf87bde07e774eca1c0
Matt Bedell <matt@bedell.co>
2022-10-17 15:42:48 -0400

    Rm deprecated nvm-cmp api

--- nvim/.config/nvim/lua/usr/plugin/lsp.lua
+++ nvim/.config/nvim/lua/usr/plugin/lsp.lua
@@ -1,6 +1,6 @@
 local nvim_lsp = require'lspconfig'
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 vim.diagnostic.config({
   underline = false,
