83b5ee3e093019f63f238e761ad635900b1e1ae1
Matt Bedell <matt@bedell.co>
2023-01-10 14:11:02 -0500

    Disable no-neck-pain plugin by default

--- nvim/.config/nvim/lua/vendor/plugin/no-neck-pain.lua
+++ nvim/.config/nvim/lua/vendor/plugin/no-neck-pain.lua
@@ -1,6 +1,6 @@
 local hi_normalnc = vim.api.nvim_get_hl_by_name('NormalNC', true)
 require("no-neck-pain").setup({
-  enableOnVimEnter = true,
+  enableOnVimEnter = false,
   width = 100,
   buffers = {
     backgroundColor = string.format('#%06x', hi_normalnc.background),
