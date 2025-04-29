8b52e98846f31bb0450944e3c51fa0fd2ae2c4ed
Matt Bedell <matt@bedell.co>
2025-04-29 19:54:28 -0400

    Fixup statusline highlight group

--- nvim/.config/nvim/lua/usr/highlights.lua
+++ nvim/.config/nvim/lua/usr/highlights.lua
@@ -19,8 +19,8 @@ local function update_highlights()
     fg = usr_util.get_hi_attr('PmenuThumb', 'bg', 'gui'),
   })
 
-  local statusline_fg = usr_util.get_hi_attr('PmenuThumb', 'bg#', 'gui')
-  vim.api.nvim_command('highlight StatusLine cterm=reverse gui=reverse guifg=' ..statusline_fg)
+  local statusline_bg = usr_util.get_hi_attr('PmenuThumb', 'bg#', 'gui')
+  vim.api.nvim_command('highlight StatusLine guibg=' ..statusline_bg)
   usr_util.extend_hi_gui('StatusLine', 'StatusLineNC', { reverse = true })
 
   usr_util.extend_hi_gui('DiagnosticError', 'stlLspError', {
