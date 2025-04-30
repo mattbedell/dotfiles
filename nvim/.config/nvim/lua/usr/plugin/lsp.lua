46eb6c613224327ff8cab81e98ba90b3a1bcdcd4
Matt Bedell <matt@bedell.co>
2025-04-29 20:04:57 -0400

    Rm deprecated diagnostic sign define

--- nvim/.config/nvim/lua/usr/plugin/lsp.lua
+++ nvim/.config/nvim/lua/usr/plugin/lsp.lua
@@ -189,19 +189,19 @@ nvim_lsp.html.setup{
   capabilities = capabilities,
 }
 
-vim.fn.sign_define("DiagnosticSignError", {
-  text = '>>',
-  texthl = "DiagnosticSignError",
-})
-vim.fn.sign_define("DiagnosticSignWarn", {
-  text = '!>',
-  texthl = "DiagnosticSignWarn",
-})
-vim.fn.sign_define("DiagnosticSignInformation", {
-  text = '<>',
-  texthl = "DiagnosticSignInfo",
-})
-vim.fn.sign_define("DiagnosticSignHint", {
-  text = '?>',
-  texthl = "DiagnosticSignHint",
+vim.diagnostic.config({
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = ">>",
+      [vim.diagnostic.severity.WARN]  = "!>",
+      [vim.diagnostic.severity.HINT]  = "?>",
+      [vim.diagnostic.severity.INFO]  = "<>",
+    },
+    texthl = {
+      [vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
+      [vim.diagnostic.severity.WARN]  = "DiagnosticSignWarn",
+      [vim.diagnostic.severity.HINT]  = "DiagnosticSignHint",
+      [vim.diagnostic.severity.INFO]  = "DiagnosticSignInformation",
+    }
+  }
 })
